eb02f8b7cc79e57174ec5e70970d2d36f7e18e64
Sandeep Ahirwar <109104353+sandiip07@users.noreply.github.com>
2023-06-17 05:10:05 -0700

    Update end.js
    yrrrrrr this was the minor mistake which i have done here .
    1 . The actual score are had not shown there because i used random value to show as a score , so now i have found the mistake and do correction , 
    correction was done in line no. 21 ; 
    put mostRecentScore at the score element;

--- end.js
+++ end.js
@@ -3,7 +3,7 @@ const saveScoreButton = document.getElementById("saveScoreButton");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores") || []);
+const highScores = JSON.parse(localStorage.getItem("highScores"))|| [];
 
 const MAX_HIGH_SCORES = 5;
 
@@ -18,7 +18,7 @@ saveHighScore = (e) => {
   e.preventDefault();
 
   const score = {
-    score: Math.floor(Math.random() * 100),
+    score: Math.floor(mostRecentScore),
     name: username.value,
   };
 
